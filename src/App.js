7721f97e671e5ce033ed999b89c8870185752258
Reuben Ayres <ra@sent.com>
2018-05-07 14:41:35 -0500

    get request to our api

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: []
+    }
+  }
+  componentDidMount = () => {
+    this.getItems()
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+  getItems = async () => {
+    const itemsJson = await fetch('http://localhost:9292/items');
+    const items = await itemsJson.json();
+    return items;
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        See Console
       </div>
     );
   }
